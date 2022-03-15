ed22402d0ecdb89c984b39e55d3ec3084b29507b
Frank Tran <44277992+fet204@users.noreply.github.com>
2022-03-15 04:23:56 +0000

    adding architecture pages

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'Architecture Home Page';
+  res.render('pages/index', {title:title});
+});
+
+app.get('/GreekRevival', (req, res) => {
+  var title = 'Greek Revival Page';
+  res.render('pages/GreekRevival', {title:title});
+});
+
+app.get('/Brutalist', (req, res) => {
+  var title = 'Brutalist Page';
+  res.render('pages/Brutalist', {title:title});
+});
+
+app.get('/HyperModern', (req, res) => {
+  var title = 'HyperModern Page';
+  res.render('pages/HyperModern', {title:title});
+});
+
+app.get('/Neo', (req, res) => {
+  var title = 'Neo Futurism Page';
+  res.render('pages/Neo', {title:title});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
