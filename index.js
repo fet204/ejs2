fde29140bf4d475cf7c68a14533334462336fa42
Frank Tran <44277992+fet204@users.noreply.github.com>
2022-03-23 17:25:25 +0000

    from class

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Here's our test data
+var data = require('./data/test.json');
+
 app.set('view engine', 'ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -11,6 +14,25 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   var title = 'Architecture Home Page';
   res.render('pages/index', {title:title});
+});
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
 });
 
 app.get('/GreekRevival', (req, res) => {
@@ -35,4 +57,5 @@ app.get('/Neo', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
